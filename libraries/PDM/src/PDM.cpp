4e6230aa36d54ae86affcc5e0931534cfcded5bb
zhangguanwen <970581469@qq.com>
2021-10-21 16:06:50 +0800

    Collect data on the rising edge of the microphone

--- libraries/PDM/src/PDM.cpp
+++ libraries/PDM/src/PDM.cpp
@@ -100,11 +100,11 @@ int PDMClass::begin(int channels, long sampleRate)
 
   switch (channels) {
     case 2:
-      nrf_pdm_mode_set(NRF_PDM, NRF_PDM_MODE_STEREO, NRF_PDM_EDGE_LEFTFALLING);
+      nrf_pdm_mode_set(NRF_PDM, NRF_PDM_MODE_STEREO, NRF_PDM_EDGE_LEFTRISING);
       break;
 
     case 1:
-      nrf_pdm_mode_set(NRF_PDM, NRF_PDM_MODE_MONO, NRF_PDM_EDGE_LEFTFALLING);
+      nrf_pdm_mode_set(NRF_PDM, NRF_PDM_MODE_MONO, NRF_PDM_EDGE_LEFTRISING);
       break;
 
     default:
