7558378643f85ea1bad4b2efab56e566b5f5c8af
zhangguanwen <970581469@qq.com>
2021-11-12 10:53:03 +0800

    Modify the demo of VBAT

--- libraries/Bluefruit52Lib/examples/Hardware/adc_vbat/adc_vbat.ino
+++ libraries/Bluefruit52Lib/examples/Hardware/adc_vbat/adc_vbat.ino
@@ -1,86 +1,27 @@
 #include <Arduino.h>
 
+#define VBAT_DIVIDER      (0.332888F)   // 1M + 0.499M voltage divider on VBAT
+#define VBAT_DIVIDER_COMP (3.004008F)        // Compensation factor for the VBAT divider
 
-#if defined ARDUINO_NRF52840_CIRCUITPLAY
-#define  PIN_VBAT          A8   // this is just a mock read, we'll use the light sensor, so we can run the test
-#endif
-
-uint32_t vbat_pin = PIN_VBAT;             // A7 for feather nRF52832, A6 for nRF52840
-
-#define VBAT_MV_PER_LSB   (0.73242188F)   // 3.0V ADC range and 12-bit ADC resolution = 3000mV/4096
-
-#ifdef NRF52840_XXAA
-#define VBAT_DIVIDER      (0.5F)          // 150K + 150K voltage divider on VBAT
-#define VBAT_DIVIDER_COMP (2.0F)          // Compensation factor for the VBAT divider
-#else
-#define VBAT_DIVIDER      (0.71275837F)   // 2M + 0.806M voltage divider on VBAT = (2M / (0.806M + 2M))
-#define VBAT_DIVIDER_COMP (1.403F)        // Compensation factor for the VBAT divider
-#endif
-
-#define REAL_VBAT_MV_PER_LSB (VBAT_DIVIDER_COMP * VBAT_MV_PER_LSB)
-
-
-float readVBAT(void) {
-  float raw;
-
-  // Set the analog reference to 3.0V (default = 3.6V)
-  analogReference(AR_INTERNAL_3_0);
-
-  // Set the resolution to 12-bit (0..4095)
-  analogReadResolution(12); // Can be 8, 10, 12 or 14
-
-  // Let the ADC settle
-  delay(1);
-
-  // Get the raw 12-bit, 0..3000mV ADC value
-  raw = analogRead(vbat_pin);
-
-  // Set the ADC back to the default settings
-  analogReference(AR_DEFAULT);
-  analogReadResolution(10);
-
-  // Convert the raw value to compensated mv, taking the resistor-
-  // divider into account (providing the actual LIPO voltage)
-  // ADC range is 0..3000mV and resolution is 12-bit (0..4095)
-  return raw * REAL_VBAT_MV_PER_LSB;
-}
-
-uint8_t mvToPercent(float mvolts) {
-  if(mvolts<3300)
-    return 0;
-
-  if(mvolts <3600) {
-    mvolts -= 3300;
-    return mvolts/30;
-  }
-
-  mvolts -= 3600;
-  return 10 + (mvolts * 0.15F );  // thats mvolts /6.66666666
-}
+int adcvalue = 0;
+float mv_per_lsb = 3600.0F/1024.0F; // 10-bit ADC with 3.6V input range
 
 void setup() {
+  pinMode(VBAT_ENABLE, OUTPUT);
+  digitalWrite(VBAT_ENABLE, LOW);
   Serial.begin(115200);
   while ( !Serial ) delay(10);   // for nrf52840 with native usb
-
-  // Get a single ADC sample and throw it away
-  readVBAT();
 }
 
 void loop() {
-  // Get a raw ADC reading
-  float vbat_mv = readVBAT();
-
-  // Convert from raw mv to percentage (based on LIPO chemistry)
-  uint8_t vbat_per = mvToPercent(vbat_mv);
+  // Get a fresh ADC value
+  adcvalue = analogRead(PIN_VBAT);
 
   // Display the results
+  Serial.print(adcvalue);
+  Serial.print(" [");
+  Serial.print((float)adcvalue * mv_per_lsb*VBAT_DIVIDER_COMP);
+  Serial.println(" mV]");
 
-  Serial.print("LIPO = ");
-  Serial.print(vbat_mv);
-  Serial.print(" mV (");
-  Serial.print(vbat_per);
-  Serial.println("%)");
-
-  delay(1000);
+  delay(100);
 }
-
